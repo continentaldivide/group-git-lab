17b5b817bbcce74657081ff373ba56194f672777
jaronw1 <jaronwong6@gmail.com>
2023-05-01 14:49:52 -0700

    header

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return (
-        <>
-        <p>I'm header</p>
-        </>
+        <nav>
+            <ul list-style-type="none">
+                <li>
+                    <Link to="/" >Home</Link>
+                </li>
+
+                <li>
+                    <Link to="/about" >About Us</Link>
+                </li>
+            </ul>
+        </nav>
     )
 }
