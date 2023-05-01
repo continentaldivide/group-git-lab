f0456ed8e9ec4c94e3bbcb6bdb39b26a557381d8
brogers-97 <brburr24@gmail.com>
2023-05-01 14:46:36 -0700

    fixed typos

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,15 +3,9 @@ import Card from '../partials/Card'
 export default function About() {
     return (
         <div className='card-container'>
-            <div className='card'>
                 <Card />
-            </div>
-            <div>
                 <Card />
-            </div>
-            <div>
                 <Card />
-            </div>
         </div>
     )
 }
