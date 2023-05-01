565bfa2da174d7ca8054312f843ec6893389704b
Andrew <andrew@andrewsmith.org>
2023-05-01 17:49:41 -0400

    Build card skeleton

--- src/components/partials/Card.jsx
+++ src/components/partials/Card.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
+import "../../Card.css"
+
 export default function Card() {
+    const [name, setName ] = useState("Andrew")
+    const [ bio, setBio ] = useState("this is Andrew's bio")
     return (
-        <>
+        <div className="Card">
         <p>I'm card</p>
-        </>
+        <p>{name}</p>
+        <p>{bio}</p>
+        </div>
     )
 }
