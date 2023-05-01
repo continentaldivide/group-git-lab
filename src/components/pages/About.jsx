329ba0f389287cbec0d46064a99ad3a1ed75c369
Andrew <andrew@andrewsmith.org>
2023-05-01 19:02:01 -0400

    Update bio

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -15,7 +15,7 @@ export default function About() {
         },
         {
             name: "Andrew",
-            bio: "c",
+            bio: "Bio placeholder",
             image: ""
         }
     ]
